1a03d8c8011badb3b007595a171dc5547d374c0a
rodrigoogalde <rjogalde@uc.cl>
2024-06-15 22:13:57 -0400

    chore: Update model associations and routes

--- src/models/likes.js
+++ src/models/likes.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Shop, { 
+      this.belongsTo(models.Shops, { 
         foreignKey: 'shop_id',
       });
       this.belongsTo(models.User, { 
--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,11 @@ dotenv.config();
 const router = new Router();
 
 // Rutas
-router.use('/territories', territories.routes());
-router.use('/auth', authentication.routes());
+// router.use('/auth', authentication.routes());
 // Rutas protegidas
 router.use(jwtMiddleware({ secret: process.env.JWT_SECRET }));
 
-router.use('/users', users.routes());
-router.use('/games', games.routes());
+// router.use('/users', users.routes());
+// router.use('/games', games.routes());
 
 module.exports = router;

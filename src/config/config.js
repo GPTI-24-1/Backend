e6064a260112ae0161270ce728db933636a23523
rodrigoogalde <rjogalde@uc.cl>
2024-06-24 21:07:59 -0400

    feat: render configs

--- src/config/config.js
+++ src/config/config.js
@@ -1,30 +1,49 @@
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: `${process.env.DB_NAME}`,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//   },
+//   test: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: `${process.env.DB_NAME}`,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//   },
+//   production: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: `${process.env.DB_NAME}`,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//   },
+// };
+
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}`,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}`,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+  }
 };
